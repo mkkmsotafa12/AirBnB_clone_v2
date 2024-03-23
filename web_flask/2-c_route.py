19b955a0a44be807a1c4b9b16f4d0529c5a3311f
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-03-23 08:01:53 -0400

    add some edit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """ disply Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ disply HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def admin():
+    """ Display 'C' followed by the value """
+    tx = tx.replace('_', ' ')
+    return f"C {escape(tx)}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
