28c4c8a016138cf99c9fd12fe01c296dde9bb763
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-03-23 08:13:58 -0400

    adding function that return a page about c

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """ disply Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ disply HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def admin(text):
+    """ Display 'C' followed by the value """
+    text = text.replace('_', ' ')
+    return f"C {escape(text)}"
+
+
+@app.route("/python/<text>", strict_sashes=False)
+@app.route("/python", strict_salshes=False)
+def admin_py(text=None):
+    """ display “Python ”, followed by the value """
+    if text:
+        text = text.replace("_", " ")
+        return f"Python {escape(text)}"
+    else:
+        return "Python is cool"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
